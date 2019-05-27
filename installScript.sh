28a0ebbb11832437964c9fefb56e84c28be4523c
Michael Albert <buzz66boy@gmail.com>
2019-05-27 12:40:54 -0600

    added IPC to compile, added install script

---
+++ installScript.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+cp libyntdl.so* /usr/local/lib
+cp src/yntdl/include/yntdl.h /usr/local/include
--- src/libtest/main.cpp
+++ src/libtest/main.cpp
@@ -7,4 +7,5 @@ using namespace std;
 
 int main() {
     cout << "it worked!" << endl;
+    yntdl::Node node();
 }
