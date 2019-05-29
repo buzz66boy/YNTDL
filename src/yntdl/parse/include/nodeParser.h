ddbe73498db60a653f91c8b1d8cbbb8b306baf83
Michael Albert <buzz66boy@gmail.com>
2019-05-28 20:52:03 -0600

    Revert "removing old header files"
    This reverts commit 3c5990760c9344f3e481e4ad088cb848d48779ea.

---
+++ src/yntdl/parse/include/nodeParser.h
@@ -0,0 +1,18 @@
+#ifndef __NODE_PARSER_H_INCLUDED__
+#define __NODE_PARSER_H_INCLUDED__
+
+// forward declared dependencies
+
+// include dependencies
+#include <vector>
+#include <memory>
+
+#include "node.h"
+#include "topologyParser.h"
+
+// declarations
+
+std::vector<std::shared_ptr<yntdl::Node> > parseNode(YAML::Node node, ParsedTopology *top);
+void parseNodeApplications(YAML::Node apps, std::shared_ptr<yntdl::Node> node);
+
+#endif
