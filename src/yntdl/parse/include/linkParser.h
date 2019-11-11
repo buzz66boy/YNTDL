14d0613737b11fb7666695c6332dedb499a0f2ed
mike <mda5893@rit.edu>
2019-11-10 22:49:50 -0700

    Revert "Removing old headers for release #noRegrats"
    This reverts commit afeede1675db92982608bdf1258ca32818a129ef.

---
+++ src/yntdl/parse/include/linkParser.h
@@ -0,0 +1,15 @@
+#ifndef __LINK_PARSER_H_INCLUDED__
+#define __LINK_PARSER_H_INCLUDED__
+
+#include <memory>
+
+#include "link.h"
+#include "topologyParser.h"
+
+bool doesLinkExist(YAML::Node node, ParsedTopology *top);
+
+void parseLink(YAML::Node link, ParsedTopology *top);
+
+void overrideLink(YAML::Node link, ParsedTopology *top);
+
+#endif
